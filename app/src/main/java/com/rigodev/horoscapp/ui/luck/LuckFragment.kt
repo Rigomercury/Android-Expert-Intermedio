8a1a0f4db8bb6c8f1376e4794ad42e9a3f500b0f
Rigomercury <rodri.aguero.7446@gmail.com>
2023-09-26 23:56:30 -0300

    luck screen completed

--- app/src/main/java/com/rigodev/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/rigodev/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,8 @@
 package com.rigodev.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +17,11 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.rigodev.horoscapp.R
 import com.rigodev.horoscapp.databinding.FragmentLuckBinding
+import com.rigodev.horoscapp.ui.core.listeners.OnSwipeTouchListener
+import com.rigodev.horoscapp.ui.model.LuckyModel
+import com.rigodev.horoscapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 import kotlin.random.Random
 
 @AndroidEntryPoint
@@ -25,17 +31,52 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var randomCardProvider: RandomCardProvider
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
+    private fun preparePrediction() {
+        val currentLuck = randomCardProvider.getLucky()
+        currentLuck?.let { luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener { shareResult(currentPrediction) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, "MI CARTA DE LA SUERTE DEL DIA: ")
+        startActivity(shareIntent)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners() {
-        binding.ivRoulette.setOnClickListener { spinRoulette() }
+        //binding.ivRoulette.setOnClickListener { spinRoulette() }
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()){
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
+
     }
 
     private fun spinRoulette() {
@@ -102,8 +143,12 @@ class LuckFragment : Fragment() {
 
             override fun onAnimationRepeat(p0: Animation?) {}
         })
+
+        val example = LuckyModel(423, R.string.aries)
+
         binding.preview.startAnimation(disappearAnimation)
         binding.prediction.startAnimation(appearAnimation)
+
     }
 
     override fun onCreateView(
